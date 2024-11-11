e75590f8ff6da1cefd7d3de9e09afbd80330889f
juryory <18516909028@163.com>
2024-11-11 20:07:45 +0800

    更新其他浏览器支持hls

--- script.js
+++ script.js
@@ -2,7 +2,25 @@ document.addEventListener("DOMContentLoaded", function () {
     const slides = document.querySelectorAll(".testimonial-slide");
     const prevButton = document.querySelector(".prev-button");
     const nextButton = document.querySelector(".next-button");
+    const video = document.getElementById("liveVideo");
+    const videoSrc = "http://www.wjchome.com:2022/live/414e4a14a874.m3u8";
     let currentIndex = 0;
+
+    // 检查浏览器是否原生支持 HLS
+    if (video.canPlayType("application/vnd.apple.mpegurl")) {
+        // 如果支持，则直接设置视频源
+        video.src = videoSrc;
+    } else if (Hls.isSupported()) {
+        // 如果不支持 HLS，则使用 hls.js 加载
+        const hls = new Hls();
+        hls.loadSource(videoSrc);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            video.play();
+        });
+    } else {
+        console.error("此浏览器不支持 HLS 播放。");
+    }
 
     function showSlide(index) {
         const slider = document.querySelector(".testimonial-slider");
